08ce5f635eba0b7504ee1abdda82e6de392243c7
Goutham Amin <gouthamamin@gmail.com>
2023-09-26 20:18:01 +0530

    Added Material Ui Image

--- src/Components/HomePageContent.tsx
+++ src/Components/HomePageContent.tsx
@@ -1,9 +1,44 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 
-function HomePageContent(){
-    return(
-        <Box>
-            <Typography variant="h4"> Welcome to Home Page of Material Ui</Typography>
+function HomePageContent() {
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "100%",
+                width: "100%",
+                padding: 0,
+                margin: 0,
+            }}
+        >
+            <Typography 
+            variant="h4"
+            sx={{
+                fontWeight:"bolder"
+            }}
+             gutterBottom>
+                Welcome to My Website
+            </Typography>
+
+            <Box
+                component="img"
+                sx={{
+                    height: 259,
+                    width: 300,
+                }}
+                alt="Material Ui Image"
+                src="./Images/MuiLogo.png"
+            />
+            <Typography variant="body1" gutterBottom>
+                Discover the <strong>amazing features</strong> of our website:
+            </Typography>
+
+            <Button variant="contained" color="primary">
+                Get Started
+            </Button>
         </Box>
     );
 }
